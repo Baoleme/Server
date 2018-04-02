fe6ef2d8bafb55c836fd95109928351a24f4b5f2
Andie <zchangan@163.com>
2018-04-02 22:46:59 +0800

    remvoe useless

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,6 +3,6 @@ ssh -i deploy_rsa root@119.29.252.110 << eeooff
 cd /root/baoleme-server
 git pull
 docker kill baoleme-server
-docker run -d --name baoleme-server --rm -v /root/baoleme-server:/server -p 8520:8520 node:alpine /bin/sh -c "ls /server && npm i /server && npm start --prefix /server"
+docker run -d --name baoleme-server --rm -v /root/baoleme-server:/server -p 8520:8520 node:alpine /bin/sh -c "npm i /server && npm start --prefix /server"
 
 eeooff
