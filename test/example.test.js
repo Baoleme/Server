1cf4017c9511ce7578c6078d476b6f3e122f09e1
Andie <zchangan@163.com>
2018-04-02 12:22:07 +0800

    test

--- test/example.test.js
+++ test/example.test.js
@@ -4,7 +4,7 @@ describe('Module', async function () {
   describe('function', async function () {
     it('case', async function () {
       const a = await new Promise((resolve, reject) => {
-        resolve(1);
+        resolve(2);
       });
       assert(a === 2);
     });
