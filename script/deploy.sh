6b87eb008976b813c8230ebeb0633fabe94eaf2d
Andie <zchangan@163.com>
2018-04-02 16:45:38 +0800

    deployment

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,8 @@
-pwd
-
-ls
-
 ssh -i deploy_rsa root@119.29.252.110 << eeooff
 
-screen -dmS travis /bin/bash
+cd /root/baoleme-server
+git pull
+docker kill baoleme-server
+docker run -d --name baoleme-server --rm -v /root/baoleme-server:/server -p 8520:8520 node:alpine /bin/sh -c "ls /server && npm i /server && npm start --prefix /server"
 
 eeooff
