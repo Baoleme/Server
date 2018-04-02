1f7884e464b7348a38d98b0cb8a0dafe442dfb39
Andie <zchangan@163.com>
2018-04-02 23:30:28 +0800

    add deploy log

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,8 +1,23 @@
+echo ---------------------------------------------
+echo             sending secret key
+echo ---------------------------------------------
 ssh -i deploy_rsa root@119.29.252.110 << eeooff
 
 cd /root/baoleme-server
+echo ---------------------------------------------
+echo                git pull
+echo ---------------------------------------------
 git pull
-docker kill baoleme-server
+echo ---------------------------------------------
+echo             delete old container
+echo ---------------------------------------------
+docker rm -f baoleme-server
+echo ---------------------------------------------
+echo              start new container
+echo ---------------------------------------------
 docker run -d --name baoleme-server --rm -v /root/baoleme-server:/server -p 8520:8520 node:alpine /bin/sh -c "npm i /server && npm start --prefix /server"
 
 eeooff
+echo ---------------------------------------------
+echo                logout Server
+echo ---------------------------------------------
