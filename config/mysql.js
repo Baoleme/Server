a816d3c9e20ea389c7c56b6a4435bcc247726119
Andie <zchangan@163.com>
2018-04-03 10:08:40 +0800

    add test database

--- config/mysql.js
+++ config/mysql.js
@@ -1,9 +1,10 @@
+const isTest = process.env.NODE_ENV === 'test';
 module.exports = {
   connectionLimit: 10,
   host: '119.29.252.110',
   port: 3306,
   user: 'root',
   password: '11929252110baolememysql',
-  database: 'baoleme',
+  database: isTest ? 'baoleme-test' : 'baoleme',
   charset: 'utf8mb4_general_ci'
 };
