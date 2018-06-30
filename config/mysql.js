e66429b4092ac67449a3bbdd21500feb88cb8987
Andie <zchangan@163.com>
2018-06-30 16:12:16 +0800

    test use local mysql

--- config/mysql.js
+++ config/mysql.js
@@ -1,10 +1,18 @@
 const isTest = process.env.NODE_ENV === 'test';
-module.exports = {
+module.exports = isTest ? {
+  connectionLimit: 10,
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'localhostmysql',
+  database: 'baoleme',
+  charset: 'utf8mb4_general_ci'
+} : {
   connectionLimit: 10,
   host: '119.29.252.110',
   port: 3306,
   user: 'root',
   password: '11929252110baolememysql',
-  database: isTest ? 'baoleme-test' : 'baoleme',
+  database: 'baoleme',
   charset: 'utf8mb4_general_ci'
 };
